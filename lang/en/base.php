61fbee93df6f7302b41ef0af4adee13782084557
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-15 16:47:26 +0330

    added selora service provider with define panelio sections in selora

---
+++ lang/en/base.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Selora Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Selora for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'description' => 'A beautifully designed sales management panel that empowers businesses to streamline their sales processes, track performance, and maximize revenue with ease.',
+
+    'sections' => [
+        'content' => [
+            'name' => 'Content',
+            'title' => 'Content Management',
+        ],
+        'sell' => [
+            'name' => 'Sell',
+            'title' => 'Sell Management',
+        ],
+        'account' => [
+            'name' => 'Accounts',
+            'title' => 'Account Management',
+        ],
+        'system' => [
+            'name' => 'System',
+            'title' => 'System Management',
+        ],
+        'report' => [
+            'name' => 'Reports',
+            'title' => 'Management Reports',
+        ],
+    ],
+
+];
---
+++ src/SeloraServiceProvider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace JobMetric\Selora;
+
+use JobMetric\Selora\Providers\EventServiceProvider;
+use JobMetric\PackageCore\Enums\RegisterClassTypeEnum;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+use JobMetric\Panelio\Facades\Panelio;
+
+class SeloraServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @throws RegisterClassTypeNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('selora')
+            ->hasConfig()
+            ->hasTranslation()
+            ->hasRoute()
+            ->registerClass('event', EventServiceProvider::class, RegisterClassTypeEnum::REGISTER())
+            ->registerClass('Selora', Selora::class, RegisterClassTypeEnum::SINGLETON());
+    }
+
+    public function afterBootPackage(): void
+    {
+        Panelio::addPanel('selora', [
+            'name' => config('selora.panel_name'),
+            'namespace' => 'JobMetric\Selora\Http\Controllers',
+            'args' => [
+                'description' => trans('selora::base.description'),
+                'icon' => 'fas fa-user-shield',
+            ],
+            'permission' => 'selora',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('selora', 'content', [
+            'name' => trans('selora::base.sections.content.name'),
+            'args' => [
+                'title' => trans('selora::base.sections.content.title'),
+                'icon' => '<i class="ki-duotone ki-abstract-26 fs-2x"><span class="path1"></span><span class="path2"></span></i>',
+            ],
+            'permission' => 'selora.content',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('selora', 'sell', [
+            'name' => trans('selora::base.sections.sell.name'),
+            'args' => [
+                'title' => trans('selora::base.sections.sell.title'),
+                'icon' => '<i class="ki-duotone ki-graph-up fs-2x"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span></i>',
+            ],
+            'permission' => 'selora.sell',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('selora', 'account', [
+            'name' => trans('selora::base.sections.account.name'),
+            'args' => [
+                'title' => trans('selora::base.sections.account.title'),
+                'icon' => '<i class="ki-duotone ki-abstract-22 fs-2x"><span class="path1"></span><span class="path2"></span></i>',
+            ],
+            'permission' => 'selora.account',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('selora', 'system', [
+            'name' => trans('selora::base.sections.system.name'),
+            'args' => [
+                'title' => trans('selora::base.sections.system.title'),
+                'icon' => '<i class="ki-duotone ki-setting-3 fs-2x"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span></i>',
+            ],
+            'permission' => 'selora.system',
+            'position' => 0,
+        ]);
+
+        Panelio::addSection('selora', 'report', [
+            'name' => trans('selora::base.sections.report.name'),
+            'args' => [
+                'title' => trans('selora::base.sections.report.title'),
+                'icon' => '<i class="ki-duotone ki-chart-simple fs-2x"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></i>',
+            ],
+            'permission' => 'selora.report',
+            'position' => 0,
+        ]);
+    }
+}
